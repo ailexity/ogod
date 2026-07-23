0f1bbc6491b4926525d95d77dcafcf7502f6f1ec
rohitdongare1919-create <rohitdongare1919@gmail.com>
2026-07-23 22:00:37 +0530

    Add new dependencies for navigation and lifecycle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -70,6 +70,10 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
