eb57b31e93d75cc95ff0a2d58cb0ae936f3ee022
atharvaarole-art <atharvaarole@gmail.com>
2026-07-26 21:27:27 +0530

    Update MapsController.js

--- backend/src/controllers/MapsController.js
+++ backend/src/controllers/MapsController.js
@@ -1,1 +1,33 @@
+'use strict';
 
+const asyncHandler = require('../utils/asyncHandler');
+const { ok } = require('../utils/apiResponse');
+const ApiError = require('../utils/ApiError');
+
+const {
+    geocodeLocation
+} = require('../services/mapsService');
+
+/**
+ * GET /api/maps/geocode?address=Mumbai
+ */
+const geocode = asyncHandler(async (req, res) => {
+    const { address } = req.query;
+
+    if (!address || !address.trim()) {
+        throw ApiError.badRequest(
+            'Address is required.'
+        );
+    }
+
+    const result = await geocodeLocation(address);
+
+    return ok(res, {
+        location: result
+    });
+});
+
+module.exports =
+{
+    geocode
+};
